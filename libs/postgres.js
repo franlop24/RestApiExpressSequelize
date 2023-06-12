c1bd4a68745e1586fd38328c9d68820179ce2cf8
franlops <franlopbri@gmail.com>
2023-06-12 12:28:59 -0600

    03 Integracion de Node Postgres

---
+++ libs/postgres.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+
+const getConnection = async () => {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'franlop24',
+    password: 'admin123',
+    database: 'my_store'
+  })
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
