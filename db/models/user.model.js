7a75c006a87428c7ed3edf7181e9ce7f4266b94a
franlops <franlopbri@gmail.com>
2023-06-26 14:28:59 -0600

    15 Resolviendo Relaciones Uno a Uno

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -32,8 +32,12 @@ const UserSchema = {
 }
 
 class User extends Model {
-  static associate(){
+  static associate(models){
     //models
+    this.hasOne(models.Customer, {
+      as: 'customer',
+      foreignKey: 'user_id'
+    });
   }
 
   static config(sequelize) {
--- services/customer.service.js
+++ services/customer.service.js
@@ -7,12 +7,21 @@ class CustomerService {
   }
 
   async create(data) {
-    const newCustomer = await models.Customer.create(data);
+    // const newUser = await models.User.create(data.user);
+    // const newCustomer = await models.Customer.create({
+    //   ...data,
+    //   userId: newUser.id
+    // });
+    const newCustomer = await models.Customer.create(data, {
+      include: ['user']
+    });
     return newCustomer;
   }
 
   async find() {
-    const rta = await models.Customer.findAll();
+    const rta = await models.Customer.findAll({
+      include: ['user']
+    });
     return rta;
   }
 
