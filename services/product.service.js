cf2d3438d49177081526c5ce0b9648084c0ac2f2
franlops <franlopbri@gmail.com>
2023-07-09 20:49:54 -0600

    21 Pagination

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,4 @@
-const { faker } = require('@faker-js/faker');
+// const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
 const { models } = require('./../libs/sequelize');
@@ -13,10 +13,16 @@ class ProductsService {
     return newProduct;
   }
 
-  async find() {
-    const products = await models.Product.findAll({
+  async find(query) {
+    const options = {
       include: ['category']
-    })
+    };
+    const { limit, offset } = query;
+    if(limit && offset){
+      options.limit = limit;
+      options.offset = offset;
+    }
+    const products = await models.Product.findAll(options)
     return products;
   }
 
